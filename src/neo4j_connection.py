4b408bbdf7ab5a89a336710a87bb8c765a4f7f2f
Filippo Garagnani <101595335+FGaragnani@users.noreply.github.com>
2025-09-18 10:59:57 +0200

    Update neo4j_connection.py

--- src/neo4j_connection.py
+++ src/neo4j_connection.py
@@ -1,9 +1,10 @@
 from py2neo import Graph
-from config import NEO4J_URI, NEO4J_USER, NEO4J_PASSWORD
+from src.config import NEO4J_URI, NEO4J_USER, NEO4J_PASSWORD
 from enum import Enum
+from typing import List, Tuple, Dict
 
-from diplomatico.graph import BoardGraph
-from diplomatico.board import Board
+from src.diplomatico.graph import BoardGraph
+from src.diplomatico.board import Board
 
 class QueryType(Enum):
     RAW = "RAW"
@@ -35,58 +36,16 @@ class Neo4JConnection:
             return True
         except Exception as e:
             return False
-
-    def create_graph_query(self, r: int, c: int):
+    
+    def clean_graph(self):
         """
-            Create a board graph with the given number of rows and columns.
-
-            :param r: The number of rows.
-            :param c: The number of columns.
+            Clean the Neo4j graph by removing all nodes and relationships.
         """
-        query = ""
-        for i in range(r):
-            for j in range(c):
-                query += f"CREATE (n_{i}_{j}:Node {{row: {i}, col: {j}}})\n"
-        board_graph: BoardGraph = BoardGraph(Board(r, c))
-        adjacency_matrix = board_graph.adjacency_matrix
-        for i in range(len(adjacency_matrix)):
-            for j in range(len(adjacency_matrix[i])):
-                if adjacency_matrix[i][j] == 1:
-                    query += f"CREATE (n_{i // c}_{i % c})-[:MOVE]->(n_{j // c}_{j % c})\n"
+        query = """
+                    MATCH (n)
+                    DETACH DELETE n
+                """
         self.run_query(query)
-
-    def hamiltonian_paths(self, r: int, c: int, query_type: QueryType = QueryType.RAW) -> int:
-        """
-            Calculate the Hamiltonian paths' number for a given board size.
-
-            :param r: The number of rows.
-            :param c: The number of columns.
-            :param query_type: The type of algorithm to run.
-            :return: The Hamiltonian paths' number.
-        """
-        if query_type == QueryType.RAW:
-            query = """ 
-                        MATCH p=(n)-[*]->(m)
-                        WHERE ALL(x IN nodes(p) WHERE size(filter(y IN nodes(p) WHERE y = x)) = 1)
-                        AND size(nodes(p)) = size((MATCH (n) RETURN count(n)).value)
-                        RETURN p
-                    """
-        elif query_type == QueryType.APOC:
-            if not self.is_apoc_installed():
-                raise RuntimeError("APOC is not installed.")
-            query = """
-                        CALL apoc.path.expandConfig(
-                        {
-                            start: (n), 
-                            relationshipFilter: ">", 
-                            uniqueness: "NODE_GLOBAL", 
-                            maxLevel: size((MATCH (n) RETURN count(n)).value)
-                        }
-                        ) YIELD path
-                        WHERE size(nodes(path)) = size((MATCH (n) RETURN count(n)).value)
-                        RETURN path
-                    """
-        return 0
 
     def run_query(self, query, parameters=None):
         """
@@ -100,7 +59,82 @@ class Neo4JConnection:
             raise RuntimeError("Neo4j server is not running.")
         return self.graph.run(query, parameters).data()
 
-if __name__ == "__main__":
-    conn = Neo4JConnection()
-    result = conn.is_apoc_installed()
-    print(result)
+class Neo4JConnectionDiplomatico(Neo4JConnection):
+    def __init__(self):
+        super().__init__()
+
+        self.board_graph: BoardGraph = BoardGraph(Board(1, 1))
+
+    def create_graph_query(self, r: int, c: int):
+        """
+            Create a board graph with the given number of rows and columns.
+
+            :param r: The number of rows.
+            :param c: The number of columns.
+        """
+        query = ""
+        for i in range(r):
+            for j in range(c):
+                query += f"CREATE (n_{i}_{j}:Node {{row: {i}, col: {j}}})\n"
+        self.board_graph = BoardGraph(Board(r, c))
+        adjacency_matrix = self.board_graph.adjacency_matrix
+        for i in range(len(adjacency_matrix)):
+            for j in range(len(adjacency_matrix[i])):
+                if adjacency_matrix[i][j] == 1:
+                    query += f"CREATE (n_{i // c}_{i % c})-[:MOVE]->(n_{j // c}_{j % c})\n"
+        self.run_query(query)
+
+    def hamiltonian_paths(self, query_type: QueryType = QueryType.RAW, just_one: bool = True) -> List:
+        """
+            Calculate the Hamiltonian paths' number for the current board.
+
+            :param query_type: The type of algorithm to run.
+            :return: The Hamiltonian paths' number.
+        """
+        parameters = {}
+        if query_type == QueryType.RAW:
+            parameters = {"pathLength": self.board_graph.board.size() - 1}
+            query = f""" 
+                        MATCH p = (n:Node)-[:MOVE*{parameters["pathLength"]}]->(m:Node)
+                            WHERE ALL(x IN nodes(p) WHERE single(y IN nodes(p) WHERE y = x))
+                        RETURN p
+                    """
+        elif query_type == QueryType.APOC:
+            if not self.is_apoc_installed():
+                raise RuntimeError("APOC is not installed.")
+            query = '''
+                        MATCH (start:Node)
+                        CALL apoc.path.expandConfig(start, {
+                            relationshipFilter: "MOVE",
+                            uniqueness: "NODE_GLOBAL",
+                            maxLevel: $pathLength
+                        }) YIELD path
+                        WHERE length(path) = $pathLength
+                            AND ALL(x IN nodes(path) WHERE single(y IN nodes(path) WHERE y = x))
+                        RETURN path
+                    '''
+            parameters = {"pathLength": self.board_graph.board.size() - 1}
+
+        query += "LIMIT 1" if just_one else ""
+        result = self.run_query(query=query, parameters=parameters)
+        return self.parse_path(result)
+
+    def parse_path(self, result: List[Dict]) -> List[Tuple[int, int]]:
+        """
+            Parse a path returned by the Neo4j query into a list of (row, col) tuples.
+
+            :param path: The path returned by the Neo4j query.
+            :return: A list of (row, col) tuples representing the path.
+        """
+        paths = []
+        for record in result:
+            path = record['p']
+            # path.nodes is a list of Node objects
+            node_coords = []
+            for node in path.nodes:
+                # Each node has 'row' and 'col' properties
+                row = node['row']
+                col = node['col']
+                node_coords.append((row, col))
+            paths.append(node_coords)
+        return paths
